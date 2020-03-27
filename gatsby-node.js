525973435ee35e49962efd16170bf0d64e1d12ed
Dave Luke <dave.luke@gmail.com>
2020-03-27 16:55:33 -0400

    removed problematic files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,33 +5,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   // eslint-disable-next-line
   const createBlogsPosts = new Promise((resolve, reject) => {
-    try {
-      graphql(`
-        {
-          allDatoCmsBlogPost {
-            nodes {
-              slug
-            }
-          }
-        }
-      `).then(res => {
-        const posts = res.data.allDatoCmsBlogPost.nodes;
-        posts.map(post => {
-          const { slug } = post;
-          createPage({
-            path: `/blog/${slug}`,
-            component: path.resolve('./src/templates/BlogPost.js'),
-            context: {
-              slug,
-            },
-          });
-        });
-        resolve();
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
+
 
   /*const createListings = new Promise((resolve, reject) => {
     try {
